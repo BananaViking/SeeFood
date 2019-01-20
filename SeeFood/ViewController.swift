684aab00beaf944ec266a91fb7ef33aac5a97d08
Banana Viking <bananavikingcoder@gmail.com>
2019-01-20 12:48:34 -0600

    implemented logic to compare selected photo to IBM results to check for specific object

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -7,10 +7,14 @@
 //
 
 import UIKit
+import VisualRecognitionV3
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     let imagePicker = UIImagePickerController()
+    let apiKey = "f5VI9DxHtrcVfeOcGyk7a0ZRqoZYOZ3hZ7N7M8cwOZlc"
+    let version = "2019-01-20"
+    var classificationResults = [String]()
     
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var imageView: UIImageView!
@@ -33,11 +37,38 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             imageView.image = image
             
             imagePicker.dismiss(animated: true, completion: nil)
+            
+            let visualRecognition = VisualRecognition(version: version, apiKey: apiKey)
+            let imageData = image.jpegData(compressionQuality: 0.01)
+            let compressedImage = UIImage(data: imageData!)
+            
+            if let finalImage = compressedImage {
+                visualRecognition.classify(image: finalImage) { (classifiedImages, error) in
+                    let classes = classifiedImages?.result?.images.first?.classifiers.first?.classes
+                    
+                    self.classificationResults = []
+                    
+                    for index in 0..<classes!.count {
+                        self.classificationResults.append((classes?[index].className)!)
+                    }
+                    print(self.classificationResults)
+                    
+                    if self.classificationResults.contains("hotdog") {
+                        DispatchQueue.main.async {
+                            self.navigationItem.title = "Hotdog!"
+                        }
+                    } else {
+                        DispatchQueue.main.async {
+                            self.navigationItem.title = "Not Hotdog!"
+                        }
+                    }
+                }
+            }else {
+                print("eror compressing image")
+            }
         } else {
             print("There was an error picking the image.")
         }
     }
-
-
 }
 
